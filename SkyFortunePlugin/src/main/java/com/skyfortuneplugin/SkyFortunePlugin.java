eeae6f69d91386bf04a636a8b6ea6e699e75ed77
PEninja <124733338+PELigma@users.noreply.github.com>
2023-08-31 17:20:15 -0400

    Add files via upload

---
+++ SkyFortunePlugin/src/main/java/com/skyfortuneplugin/SkyFortunePlugin.java
@@ -0,0 +1,17 @@
+package com.skyfortuneplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyFortunePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
